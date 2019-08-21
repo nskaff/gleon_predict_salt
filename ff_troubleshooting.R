8c359742ef86db4d40453f3b48837861d698a21b
nskaff <nicholas.skaff@gmail.com>
2019-08-21 15:54:57 -0700

    3d plots

--- ff_troubleshooting.R
+++ ff_troubleshooting.R
@@ -1,12 +1,37 @@
-#load('4.2RF_Environment.RData')
+load('4.2RF_Environment.RData')
+library(forestFloor)
+library(ggplot2)
+library(lattice)
 
 rf_model_test<-ranger(dependent.variable.name='logChloride',
                  data=data.frame(logChloride=dat_rf$logChloride,rf_cov),
-                 keep.inbag = TRUE, quantreg = T, importance = "impurity")
+                 keep.inbag = TRUE, importance = "impurity", inbag=random_lake_samps,
+                 num.trees=ntree)
 ff_rf_model <- ranger_RFadaptor(rf_model,dat_rf$logChloride)
 
 ffra = forestFloor(ff_rf_model,rf_cov,calc_np = T)
 
-View(data.frame(ff=rowSums(ffra$FCmatrix)+mean(rf_model_test$predicted),pred=rf_model_test$predicted))
+View(data.frame(ff=rowSums(ffra$FCmatrix)+mean(ff_rf_model$predicted),pred=ff_rf_model$predicted))
 
 plot(ffra,plot_seq = 1:9, orderByImportance = T)
+
+Col = fcol(ffra,1)
+
+#med low dev
+show3d(ffra, 1:2, 1:2, plot_GOF=T, col = Col)
+
+#low dev crop
+show3d(ffra, c(1,3), c(1,3), plot_GOF=T, col = Col)
+
+ff_dat<-data.frame(ffra$X, ffra$FCmatrix)
+
+sum_med_low_dev<-ffra$FCmatrix[,"WS.Dev.Med"]+ffra$FCmatrix[,"WS.Dev.Low"]
+
+sum_low_dev_crop<-ffra$FCmatrix[,"WS.Dev.Med"]+ffra$FCmatrix[,"WS.Crops"]
+
+ggplot()+ geom_point( aes(x=ffra$X$WS.Dev.Low, y=ffra$X$WS.Crops, color = sum_low_dev_crop)) + 
+  scale_color_distiller(palette='Spectral') +
+  theme_bw()
+
+
+
